66fdcefdba434fdb00c7c3ac6b0b1328433068db
gouri <gdeo30@gmail.com>
2024-05-30 13:04:45 +0530

    Uploaded files

---
+++ jx6634/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
+
+export default function App() {
+  return (
+    <div className="App">
+      <DisplayDifficulty />
+    </div>
+  );
+}
